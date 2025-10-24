b9a284d59691cd94393c4a544fa87829407f9809
ouzayr-droid <salifououzerou25@gmail.com>
2025-10-24 23:05:59 +0200

    création des middleware app.get et app.post

--- server.js
+++ server.js
@@ -1,43 +1,46 @@
 const http = require("http");
 const app = require("./app");
 
-/**
- * quand on lance un serveur le port peut venir de différente sources
- * la fonction ci-dessous permet de s'assurer que le port qu'on à reçu d'une source respecte bien les normes
+/*
+ * reçoit en paramètre la valeur du port à normaliser
+ * convertir cette valeur en entier pour s'assurer qu'elle correspond bien au format normal de la valeur d'un port
+ * si après conversion, port n'est pas un nombre alors il faudra retourner value comme reçu
+ * si après conversion port est supérieur ou égal à zero alors tu retourne la valeur de port (value convertit) car elle correspond à ce qu'on attend
+ * si non default, tu retourne false
  */
-const normalizePort = (val) => {
-    const port = parseInt(val, 10);
+const normalizePort = (value) => {
+    const port = parseInt(value, 10);
+
     if (isNaN(port)) {
-        return val; // cas où ce n'est pas un nombre
+        return value;
     }
 
     if (port >= 0) {
-        return port; // valeur valide
+        return port;
     }
 
-    return false; // sinon valeur invalide
+    return false;
 };
 
-const port = normalizePort(process.env.PORT || 3000);
-// on fait la normalisation de la valeur qu'on reçoit pour le port pour cette ligne
-app.set("port", port);
-
 const errorHandler = (error) => {
-    if (error.syscall !== "listen") {
+    if (error.syscall != "listen") {
         throw error;
     }
+
     const address = server.address();
     const bind =
-        typeof address === "string" ? "pipe" + address : "port:" + port;
+        typeof address === "string" ? "pipe: " + address : "port: " + port;
 
     switch (error.code) {
-        case "EACCES":
-            console.error(bind + " requries elevated privileges.");
+        case "EACCESS":
+            console.error(bind + " vous n'avez les privilèges requis.");
             process.exit(1);
             break;
 
         case "EADDRINUSE":
-            console.error(bind + " is already in user");
+            console.error(
+                bind + " cette addresse est déjà en cours d'utilisation."
+            );
             process.exit(1);
             break;
 
@@ -46,14 +49,17 @@ const errorHandler = (error) => {
     }
 };
 
+const port = normalizePort(process.env.PORT || 3000);
+app.set("port", port);
+
 const server = http.createServer(app);
 
 server.on("error", errorHandler);
 server.on("listening", () => {
     const address = server.address();
     const bind =
-        typeof address === "string" ? "pipe " + address : "port " + port;
-    console.log("listening on " + bind);
-});
+        typeof address === "string" ? "pipe: " + address : "port: " + port;
 
+    console.log("Votre server a bien été lancé. " + bind);
+});
 server.listen(port);
